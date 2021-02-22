e76b7b991352189185e679bcfa0378fb823402b9
evantanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2021-02-22 20:06:56 +0700

    adding firebase

--- app/src/main/java/com/elflin/petqper/LoginActivity.java
+++ app/src/main/java/com/elflin/petqper/LoginActivity.java
@@ -2,13 +2,35 @@ package com.elflin.petqper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button register_login;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initComponent();
+        setListener();
+    }
+
+    private void setListener() {
+        this.register_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initComponent() {
+        this.register_login = findViewById(R.id.register_login);
     }
 }
--- app/src/main/java/com/elflin/petqper/RegisterActivity.java
+++ app/src/main/java/com/elflin/petqper/RegisterActivity.java
@@ -3,12 +3,36 @@ package com.elflin.petqper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageButton backbutton_form;
+    private TextInputLayout name_form, username_form, email_form, password_form, confirmpass_form;
+    private Button submit_form;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initComponent();
+        setListener();
+    }
+
+    private void initComponent() {
+        this.backbutton_form = findViewById(R.id.backbutton_form);
+    }
+
+    private void setListener() {
+        this.backbutton_form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
